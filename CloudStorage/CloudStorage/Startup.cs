819edc9bdbf22cfd5a6b1e3ef51fb84dda278206
Emil Craciun <emil.craciun@live.com>
2017-02-04 16:19:07 +0200

    Created a service component that gets the greeting message from the configuration file

--- CloudStorage/CloudStorage/Startup.cs
+++ CloudStorage/CloudStorage/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
+using CloudStorage.Services;
+using CloudStorage.Services.Implementation;
 
 namespace CloudStorage
 {
@@ -30,10 +32,15 @@ namespace CloudStorage
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(  IApplicationBuilder app, 
+                                IHostingEnvironment env, 
+                                ILoggerFactory loggerFactory,
+                                IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -44,7 +51,7 @@ namespace CloudStorage
 
             app.Run(async (context) =>
             {
-                var message = Configuration["GreetingMessage"];
+                var message = greeter.GetGreeting();
                 await context.Response.WriteAsync(message);
             });
         }
