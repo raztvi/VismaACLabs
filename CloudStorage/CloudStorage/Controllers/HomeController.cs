99a11378a1991f38b9138a196173f4a0c959ec7c
Emil Craciun <emil.craciun@live.com>
2017-02-04 17:02:20 +0200

    Model class

--- CloudStorage/CloudStorage/Controllers/HomeController.cs
+++ CloudStorage/CloudStorage/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CloudStorage.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +14,8 @@ namespace CloudStorage.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return Content($"Hello from {nameof(HomeController)}");
+            var model = new FileInfo { Id = Guid.NewGuid(), FileName = "bla.txt", FileSizeInBytes = 2 };
+            return new ObjectResult(model);
         }
     }
 }
---
+++ CloudStorage/CloudStorage/Models/FileInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudStorage.Models
+{
+    public class FileInfo
+    {
+        public Guid Id { get; set; }
+        public string FileName { get; set; }
+        public int FileSizeInBytes { get; set; }
+    }
+}
