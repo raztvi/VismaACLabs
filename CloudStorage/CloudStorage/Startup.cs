9c7352e13511b168e14222e9c84b08ba52919602
Emil Craciun <emil.craciun@live.com>
2017-02-04 16:40:17 +0200

    Static files & default files

--- CloudStorage/CloudStorage/Startup.cs
+++ CloudStorage/CloudStorage/Startup.cs
@@ -56,6 +56,9 @@ namespace CloudStorage
                 });
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseWelcomePage(new WelcomePageOptions
             {
                 Path = "/welcome"
