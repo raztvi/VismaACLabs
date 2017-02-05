a9f63eb7fd4e857ceb166448c745e219202e45b5
Emil Craciun <emil.craciun@live.com>
2017-02-05 18:30:37 +0200

    EF Migrations

---
+++ CloudStorage/Data/Migrations/20170205162434_InitialCreate.Designer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Data;
+using Core.Entities;
+
+namespace Data.Migrations
+{
+    [DbContext(typeof(CloudStorageDbContext))]
+    [Migration("20170205162434_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Core.Entities.FileInfo", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("ContentType");
+
+                    b.Property<string>("FileName");
+
+                    b.Property<int>("FileSizeInBytes");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("FileInfos");
+                });
+        }
+    }
+}
---
+++ CloudStorage/Data/Migrations/CloudStorageDbContextModelSnapshot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Data;
+using Core.Entities;
+
+namespace Data.Migrations
+{
+    [DbContext(typeof(CloudStorageDbContext))]
+    partial class CloudStorageDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Core.Entities.FileInfo", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("ContentType");
+
+                    b.Property<string>("FileName");
+
+                    b.Property<int>("FileSizeInBytes");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("FileInfos");
+                });
+        }
+    }
+}
