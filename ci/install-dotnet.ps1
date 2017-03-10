69d74adbb35c22d2e6ec1f2dfc310d794219ca45
Emil Craciun <emil.craciun@live.com>
2017-03-10 19:50:32 +0200

    Testing out forcing installation of newest version of dotnet core on the build agent

---
+++ ci/install-dotnet.ps1
@@ -0,0 +1,21 @@
+Write-Output "Force installing dotnet..."
+
+$dotnetPs1Path = "$PSScriptRoot\Tools"
+if (-not (Test-Path -PathType Container $dotnetPs1Path))
+{
+    New-Item -ItemType Directory -Path $dotnetPs1Path
+}
+
+$dotnetPs1Path = "$dotnetPs1Path\dotnet-install.ps1"
+
+$webClient = New-Object System.Net.WebClient
+$webClient.Proxy = [System.Net.WebRequest]::DefaultWebProxy
+$webClient.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+Write-Output "    Downloading dotnet-install.ps1 to $dotnetPs1Path"
+$webClient.DownloadFile("https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1", $dotnetPs1Path)
+
+$dotnetPath = $dotnetPs1Path
+
+Write-Output "    Calling: $dotnetPath"
+& "$dotnetPath"
+Write-Output "    Done."
